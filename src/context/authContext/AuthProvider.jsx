27216c0ab04099b3aa8c1322128152774b06a0d8
tahmidman20 <tahmid7282@gmail.com>
2026-02-16 16:20:32 +0700

    added register,signin ,google signin ,loguot

--- src/context/authContext/AuthProvider.jsx
+++ src/context/authContext/AuthProvider.jsx
@@ -1,15 +1,49 @@
-import React, { Children } from "react";
+import React, { Children, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = () => {
   const registerUSer = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  const signinUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signinGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    // setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      //   setUser(currentUser);
+      // setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
   const authInfo = {
     registerUSer,
+    signinUser,
+    signinGoogle,
+    logOut,
   };
 
   return <AuthContext value={authInfo}>{Children}</AuthContext>;
