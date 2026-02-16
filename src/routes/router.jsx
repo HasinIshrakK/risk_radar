ce980203c101c4a54fe2b2930dbb54d762261816
HasinIshrakK <hasinishrakk@gmail.com>
2026-02-16 17:05:44 +0600

    Navbar image and router fixed

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Transactions from "../pages/Transactions/Transactions";
+import Alerts from "../pages/Alerts/Alerts";
+import Reports from "../pages/Reports/Reports";
 import Home from "../pages/Home/Home";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import Loader from "../components/SharedUi/Loader";
@@ -18,7 +22,25 @@ export const router = createBrowserRouter([
       </>
     ),
     hydrateFallbackElement: <Loader />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>,
+      },
+      {
+        path: "/transaction",
+        element: <Transactions></Transactions>,
+      },
+      {
+        path: "/alerts",
+        element: <Alerts></Alerts>,
+      },
+      {
+        path: "/reports",
+        element: <Reports></Reports>,
+      },
+    ],
   },
   {
     path: "/auth",
@@ -34,9 +56,7 @@ export const router = createBrowserRouter([
         path: "register",
         element: <Register />,
       },
-
     ],
-
   },
   { path: "/*", element: <ErrorPage /> },
 ]);
